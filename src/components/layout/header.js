a1e66f8613b1b74fcf7c840b7021bfbdc20aecc5
MOTORIT\mtrsteber <stellan.bergstrom@motorit.se>
2023-11-27 19:08:25 +0100

    Fixed menu

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../contexts/userProvider";
@@ -6,44 +6,102 @@ import Logout from "../user/logout";
 
 const Header = () => {
   const { user } = useContext(UserContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isTransitionComplete, setIsTransitionComplete] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    if (!isMenuOpen) {
+      setTimeout(() => {
+        setIsTransitionComplete(true);
+      }, 500); // Adjust the timeout to match your transition duration
+    } else {
+      setIsTransitionComplete(false);
+    }
+  };
 
   return (
-    <header className="header">
-      <div className="header-brand">
-        <Link to="/">EasySetup</Link>
+    <>
+      <header className="header">
+        <div className="header-brand">
+          <Link to="/">EasySetup</Link>
+        </div>
+        <nav className="header-nav">
+          {user ? (
+            <>
+              <Link
+                to="/user/profile"
+                className="nav-link"
+              >
+                Profile
+              </Link>
+              <Logout />
+            </>
+          ) : (
+            <>
+              <Link
+                to="/register"
+                className="nav-link"
+              >
+                Sign up
+              </Link>
+              <Link
+                to="/login"
+                className="nav-link"
+              >
+                Sign in
+              </Link>
+            </>
+          )}
+        </nav>
+        <div
+          className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </header>
+      <div className={`overlay ${isMenuOpen ? "open" : ""}`}>
+        <div
+          className={`overlay-content ${isTransitionComplete ? "visible" : ""}`}
+        >
+          {user ? (
+            <>
+              <Link
+                to="/user/profile"
+                onClick={toggleMenu}
+                className="nav-link mobile-menu"
+                tabIndex="-1" /* Prevents the link from being focusable */
+              >
+                Profile
+              </Link>
+              <Logout />
+            </>
+          ) : (
+            <>
+              <Link
+                to="/register"
+                onClick={toggleMenu}
+                className="nav-link mobile-menu"
+                tabIndex="-1"
+              >
+                Sign up
+              </Link>
+              <Link
+                to="/login"
+                onClick={toggleMenu}
+                className="nav-link mobile-menu"
+                tabIndex="-1"
+              >
+                Sign in
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-      <nav className="header-nav">
-        {user && (
-          <>
-            <Link
-              to="/user/profile"
-              className="nav-link"
-            >
-              Profile
-            </Link>
-            <Logout />
-          </>
-        )}
-
-        {!user && (
-          <Link
-            to="/register"
-            className="nav-link"
-          >
-            Sign up
-          </Link>
-        )}
-
-        {!user && (
-          <Link
-            to="/login"
-            className="nav-link"
-          >
-            Sign in
-          </Link>
-        )}
-      </nav>
-    </header>
+    </>
   );
 };
 
